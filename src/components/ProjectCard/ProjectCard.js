3cc324aecea8418f7bc0c3d437d2820442a9ba59
Alexisbonk <69346752+alexisbonk@users.noreply.github.com>
2023-09-22 18:21:36 -0400

    Some fixes ⚙️

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -3,6 +3,7 @@ import ProjectLanguages from "../projectLanguages/ProjectLanguages";
 import "./ProjectCard.css";
 import { Fade } from "react-reveal";
 import { style } from "glamor";
+import { OverlayTrigger, Tooltip } from "react-bootstrap";
 
 export default function ProjectCard({ repo, theme, language }) {
   function openRepoinNewTab(url) {
@@ -10,12 +11,23 @@ export default function ProjectCard({ repo, theme, language }) {
     win.focus();
   }
 
+  function getStatusClass(status) {
+    switch (status) {
+      case true:
+        return 'status-online';
+      case false:
+        return 'status-closed';
+      default:
+        return 'status-unknown';
+    }
+  }
+
   const styles = style({
     color: "rgb(88, 96, 105)",
     backgroundColor: "rgb(255, 255, 255)",
     boxShadow: "rgba(0, 0, 0, 0.2) 0px 10px 30px -15px",
     padding: "2rem",
-    cursor: "pointer",
+    cursor: repo.online || repo.online === null ? 'pointer' : '',
     borderRadius: "5px",
     height: "100%",
     transition: "all 0.2s ease-in-out",
@@ -30,10 +42,21 @@ export default function ProjectCard({ repo, theme, language }) {
         <div
           {...styles}
           key={repo.id}
-          onClick={() => openRepoinNewTab(repo.url)}
+          onClick={() => repo.url && openRepoinNewTab(repo.url)}
           style={{ backgroundColor: theme.projectCard }}
         >
           <div className="repo-name-div">
+            <OverlayTrigger
+              key={repo.id}
+              placement={"top"}
+              overlay={
+                <Tooltip id={`tooltip-top`}>
+                  <strong>{repo.online || repo.online === null ? 'Online' : 'Offline'}</strong>
+                </Tooltip>
+              }
+            >
+              <div className={`status-circle ${getStatusClass(repo.online)}`}/>
+            </OverlayTrigger>
             <p className="repo-name" style={{ color: theme.text }}>
               {language === 'en' ? repo.name : repo.name_fr}
             </p>
